f3ce4acb423645cb8be5955f8129d38e220751ca
MaximumG9 <maximumg999@gmail.com>
2025-09-13 03:46:40 +0100

    remove a few useless things in TridentEntityMixin

--- src/main/java/com/maximumg9/shadow/mixins/TridentEntityMixin.java
+++ src/main/java/com/maximumg9/shadow/mixins/TridentEntityMixin.java
@@ -11,15 +11,12 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.TridentEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.hit.BlockHitResult;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(TridentEntity.class)
 public abstract class TridentEntityMixin {
-    @Shadow protected abstract void onBlockHitEnchantmentEffects(ServerWorld world, BlockHitResult blockHitResult, ItemStack weaponStack);
 
     @Redirect(
         method = "onEntityHit",
