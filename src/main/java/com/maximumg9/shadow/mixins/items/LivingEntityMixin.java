83e6cec8188b4da1977518e870f6c8d41bd5f01e
maximumg9 <maximumg999@gmail.com>
2025-12-31 20:33:54 -0800

    fix trident being broken, just generally broken.

--- src/main/java/com/maximumg9/shadow/mixins/items/LivingEntityMixin.java
+++ src/main/java/com/maximumg9/shadow/mixins/items/LivingEntityMixin.java
@@ -26,7 +26,8 @@ public abstract class LivingEntityMixin extends Entity {
         super(type, world);
     }
 
-    @Inject(method = "getEquipmentChanges", at = @At(value = "TAIL"))
+    // Increase order so trident mixin applies first
+    @Inject(method = "getEquipmentChanges", at = @At(value = "TAIL"), order = 1100)
     private void areItemsDifferent(CallbackInfoReturnable<Map<EquipmentSlot, ItemStack>> cir) {
         if(cir.getReturnValue() == null) return;
         cir.getReturnValue().entrySet().removeIf((entry) -> {
@@ -35,7 +36,7 @@ public abstract class LivingEntityMixin extends Entity {
         });
     }
 
-    @Redirect(method = "getEquipmentChanges",at= @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isEmpty()Z"))
+    @Redirect(method = "getEquipmentChanges",at= @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isEmpty()Z",ordinal = 1))
     public boolean dontUpdateAttributeModifiers(ItemStack oldStack) {
         return oldStack.isEmpty() || NBTUtil.getCustomData(oldStack).getBoolean(NBTUtil.DISABLE_ATTRIBUTES_KEY);
     }
