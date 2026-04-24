9363eecc1a6f21f75fc6917a01b4c5389454cf0f
BoredYoshi <lukerm17645@gmail.com>
2026-04-24 03:51:41 -0400

    roleguess now removes 3 hearts per misguess as well. removed deprecated temporary shadow

--- src/main/java/com/maximumg9/shadow/abilities/RoleGuess.java
+++ src/main/java/com/maximumg9/shadow/abilities/RoleGuess.java
@@ -14,6 +14,9 @@ import com.maximumg9.shadow.util.TextUtil;
 import com.maximumg9.shadow.util.indirectplayer.CancelPredicates;
 import com.maximumg9.shadow.util.indirectplayer.IndirectPlayer;
 import net.minecraft.component.DataComponentTypes;
+import net.minecraft.entity.attribute.EntityAttributeInstance;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.registry.RegistryWrapper;
@@ -29,6 +32,8 @@ public class RoleGuess extends Ability {
     public static final Identifier ID = MiscUtil.shadowID("role_guess");
     private static final ItemStack ITEM_STACK;
     private static final Text NAME = TextUtil.withColour("Role Guess",Formatting.DARK_PURPLE);
+    private static final Identifier HP_ATTR_ID = MiscUtil.shadowID("guess_role_max_health");
+    private static final double healthIncrease = -6;
     
     private static final int COOLDOWN_TIME = 8 * 60 * 20;
     
@@ -46,7 +51,9 @@ public class RoleGuess extends Ability {
                 TextUtil.gray("number of non-shadows alive is less than"),
                 TextUtil.gray("or equal to the number of shadows alive."),
                 TextUtil.blue("⌛ 8 minute cooldown"),
-                TextUtil.red("IF YOU GUESS INCORRECTLY, YOU DIE")
+                TextUtil.red("IF YOU GUESS INCORRECTLY, YOU LOSE STRENGTH FOR 8 MINUTES.")
+                    .styled(style -> style.withBold(true)),
+                TextUtil.red("ADDITIONALLY, YOU WILL LOSE ").append(TextUtil.hearts(6))
                     .styled(style -> style.withBold(true)),
                 AbilityText()
             )
@@ -114,12 +121,44 @@ public class RoleGuess extends Ability {
                                     );
                                 } else {
                                     pl.sendMessage(TextUtil.red("You guessed your target's role incorrectly!"));
-
-                                    if (this.player.role.hasAbility(ToggleStrength.ID)) {
+                                    if (pl.getMaxHealth() <= 6) {
+                                        this.player.damageNow(
+                                            pl.getServerWorld()
+                                                .getDamageSources()
+                                                .magic(),
+                                            Float.MAX_VALUE
+                                        );
+                                    } else if (this.player.role.hasAbility(ToggleStrength.ID)) {
                                         this.player.role.removeAbility(this.player.role.getAbility(ToggleStrength.ID).get());
+                                        EntityAttributeInstance instance = pl.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH);
+                                        if (instance != null) {
+
+                                            EntityAttributeModifier healthModifier = instance.getModifier(HP_ATTR_ID);
+                                            if (healthModifier == null) {
+                                                instance.addPersistentModifier(
+                                                    new EntityAttributeModifier(
+                                                        HP_ATTR_ID,
+                                                        healthIncrease,
+                                                        EntityAttributeModifier.Operation.ADD_VALUE
+                                                    )
+                                                );
+                                            } else {
+                                                if (healthModifier.operation() != EntityAttributeModifier.Operation.ADD_VALUE) {
+                                                    MiscUtil.getShadow(this.player.getShadow().getServer()).ERROR("Guess Role Operation isn't ADD_VALUE (how did you MANAGE this)");
+                                                    return;
+                                                }
+                                                instance.overwritePersistentModifier(
+                                                    new EntityAttributeModifier(
+                                                        HP_ATTR_ID,
+                                                        healthIncrease + healthModifier.value(),
+                                                        EntityAttributeModifier.Operation.ADD_VALUE
+                                                    )
+                                                );
+                                            }
+                                        }
                                         CancellableDelay.of(
                                             () -> {
-                                                this.player.role.addAbility(this.player, ToggleStrength::new);
+                                                this.player.role.addAbility(ToggleStrength::new);
                                             },
                                             COOLDOWN_TIME,
                                             CancellableDelay.wrapCancelCondition(CancelPredicates.cancelOnLostRole(this.player.role), this.player)
